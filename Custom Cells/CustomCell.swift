47b5df23649a2c894ad7c4efd1dd918ff4ddabc8
In Soo Yang <isyang1223@gmail.com>
2018-04-08 13:25:47 -0700

    initial commit

---
+++ Custom Cells/CustomCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomCell.swift
+//  Custom Cells
+//
+//  Created by Ian Yang on 3/14/18.
+//  Copyright © 2018 Ian Yang. All rights reserved.
+//
+
+import UIKit
+class CustomCell: UITableViewCell {
+    
+    @IBOutlet weak var leftButton: UIButton!
+    
+    @IBOutlet weak var rightLabel: UILabel!
+}
